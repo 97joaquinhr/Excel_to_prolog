d3ebdf0125adc684f6f46119082c3ea8883c1a62
97joaquinhr <97joaquinhr@gmail.com>
2019-11-04 19:31:15 -0600

    Read from excel and write to txt

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,5 +1,62 @@
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.openxmlformats.schemas.drawingml.x2006.main.ThemeDocument;
+//https://www.callicoder.com/java-read-excel-file-apache-poi/
 public class Main {
-    public static void main(String [] args){
-        System.out.println("Hello world");
+    private static final String SAMPLE_XLSX_FILE_PATH = "C:\\Users\\joaqu\\Documents\\Codigo\\Excel_to_prolog\\data\\Proyecto.xlsx";
+
+    public static void main(String[] args) throws IOException, InvalidFormatException {
+
+        // Creating a Workbook from an Excel file (.xls or .xlsx)
+        Workbook workbook = WorkbookFactory.create(new File(SAMPLE_XLSX_FILE_PATH));
+
+        // Retrieving the number of sheets in the Workbook
+        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+
+        /*
+           Iterating over all the sheets in the workbook (Multiple ways)
+        */
+
+        // use a Java 8 forEach with lambda
+        System.out.println("Retrieving Sheets using Java 8 forEach with lambda");
+        workbook.forEach(sheet -> {
+            System.out.println("=> " + sheet.getSheetName());
+        });
+
+        /*
+           Iterating over all the rows and columns in a Sheet (Multiple ways)
+        */
+
+        // Getting the Sheet at index zero
+        Sheet sheet = workbook.getSheetAt(0);
+
+        // Create a DataFormatter to format and get each cell's value as String
+        DataFormatter dataFormatter = new DataFormatter();
+
+        for (Row row: sheet) {
+            for(Cell cell: row) {
+                String cellValue = dataFormatter.formatCellValue(cell);
+                System.out.print(cellValue + "\t");
+            }
+            System.out.println();
+        }
+
+        // Closing the workbook
+        workbook.close();
+
+        /*writing to file*/
+        List<String> lines = Arrays.asList("The first line", "The second line");
+        Path file = Paths.get("the-file-name.txt");
+        Files.write(file, lines, StandardCharsets.UTF_8);
     }
 }
