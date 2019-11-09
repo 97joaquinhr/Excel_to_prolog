768097cea635f6a72245448f9f8c94199629a57a
97joaquinhr <97joaquinhr@gmail.com>
2019-11-09 14:18:29 -0600

    Commented with ToDo recursiveactions

--- java/src/Main.java
+++ java/src/Main.java
@@ -50,6 +50,7 @@ public class Main {
         }
     }
 
+    //TODO: Recursive Action
     private static void process(String[][] data,int n,int m,String[] relations){
         int i,j;
         for (i = 0; i < n; ++i){
@@ -59,7 +60,7 @@ public class Main {
                 } else {
                     data[i][j] = data[i][j].toLowerCase();
                 }
-                lines.add(relations[i]+"("+CRNs[j]+","+data[i][j]+").");
+                data[i][j]=relations[i]+"("+CRNs[j]+","+data[i][j]+").";
             }
         }
     }
@@ -72,13 +73,14 @@ public class Main {
         String[][] data = new String[n_rows_to_read][sheet.getLastRowNum()];
         create_relations(rows_to_read,relations,n_rows_to_read);
 
-
         String relation="";
         int i=0,j;
         lines.add(":- encoding(utf8).");
         for(i=0; i<n_rows_to_read;++i){
             lines.add(":- discontiguous "+relations[i]+"/2.");
         }
+
+        //read
         i=0;
         for(Row row: sheet){
             if(i>=1){
@@ -89,10 +91,20 @@ public class Main {
             }
             i++;
         }
+
+        //process data
         process(data,n_rows_to_read,sheet.getLastRowNum(),relations);
-        workbook.close();
-        /*writing to file*/
+
+        //write
+        for (i = 0; i < n_rows_to_read; ++i){
+            for(j=0;j<sheet.getLastRowNum();++j) {
+                lines.add(data[i][j]);
+            }
+        }
         Path file = Paths.get("C:\\Users\\joaqu\\Documents\\Codigo\\Excel_to_prolog\\data\\data.pl");
         Files.write(file, lines, StandardCharsets.UTF_8);
+
+        //close
+        workbook.close();
     }
 }
