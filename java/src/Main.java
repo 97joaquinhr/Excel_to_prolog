ad3abd0583dfa8ba226dfd8adbad7eab88f6da22
97joaquinhr <97joaquinhr@gmail.com>
2019-11-09 14:11:52 -0600

    modularized processing data

--- java/src/Main.java
+++ java/src/Main.java
@@ -17,6 +17,8 @@ public class Main {
     private static final String SAMPLE_XLSX_FILE_PATH = "C:\\Users\\joaqu\\Documents\\Codigo\\Excel_to_prolog\\data\\Proyecto.xlsx";
     // Creating a Workbook from an Excel file (.xls or .xlsx)
     private static Workbook workbook;
+    private static List<String> lines= new ArrayList<String>();
+
     private static DataFormatter dataFormatter = new DataFormatter();
     static {
         try {
@@ -26,6 +28,7 @@ public class Main {
         }
     }
     private static Sheet sheet = workbook.getSheetAt(0);
+    private static String[] CRNs = new String[sheet.getLastRowNum()];
 
     private static String decapitalize(String string){
         if (string == null || string.length() == 0) {
@@ -46,39 +49,47 @@ public class Main {
             relations[i]= CRN +"_"+camelCase(aux);
         }
     }
+
+    private static void process(String[][] data,int n,int m,String[] relations){
+        int i,j;
+        for (i = 0; i < n; ++i){
+            for(j=0;j<m;++j){
+                if (data[i][j].contains(" ")||data[i][j].contains("+")||data[i][j].contains("@")) {
+                    data[i][j] = "\"" + data[i][j] + "\"";
+                } else {
+                    data[i][j] = data[i][j].toLowerCase();
+                }
+                lines.add(relations[i]+"("+CRNs[j]+","+data[i][j]+").");
+            }
+        }
+    }
     public static void main(String[] args) throws IOException, InvalidFormatException {
 
 
         int[] rows_to_read = new int[]{7,19,20};
         int n_rows_to_read =rows_to_read.length;
         String[] relations = new String[n_rows_to_read];
-        //String[] data = new String[n_rows_to_read];
+        String[][] data = new String[n_rows_to_read][sheet.getLastRowNum()];
         create_relations(rows_to_read,relations,n_rows_to_read);
 
-        List<String> lines= new ArrayList<String>();
+
         String relation="";
         int i=0,j;
-        String aux;
         lines.add(":- encoding(utf8).");
         for(i=0; i<n_rows_to_read;++i){
             lines.add(":- discontiguous "+relations[i]+"/2.");
         }
         i=0;
         for(Row row: sheet){
-            if(i>1){
-                String crn1 = dataFormatter.formatCellValue(row.getCell(CRN_row));
+            if(i>=1){
+                CRNs[i-1] = dataFormatter.formatCellValue(row.getCell(CRN_row));
                 for(j=0;j<n_rows_to_read;++j) {
-                    aux = dataFormatter.formatCellValue(row.getCell(rows_to_read[j]));
-                    if (aux.contains(" ")) {
-                        aux = "\"" + aux + "\"";
-                    } else {
-                        aux = aux.toLowerCase();
-                    }
-                    lines.add(relations[j] + "("+crn1 + "," + aux + ").");
+                    data[j][i-1] = dataFormatter.formatCellValue(row.getCell(rows_to_read[j]));
                 }
             }
             i++;
         }
+        process(data,n_rows_to_read,sheet.getLastRowNum(),relations);
         workbook.close();
         /*writing to file*/
         Path file = Paths.get("C:\\Users\\joaqu\\Documents\\Codigo\\Excel_to_prolog\\data\\data.pl");
