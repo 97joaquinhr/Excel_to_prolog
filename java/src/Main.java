9cae56a10a578ae599dcd150f6214705a4fd9877
97joaquinhr <97joaquinhr@gmail.com>
2019-11-10 09:30:30 -0600

    refactored some variables

--- java/src/Main.java
+++ java/src/Main.java
@@ -13,13 +13,9 @@ import java.util.concurrent.ForkJoinPool;
 
 //https://www.callicoder.com/java-read-excel-file-apache-poi/
 public class Main {
-    private static int CRN_row = 5;
-    private static String CRN = "crn";
     private static final String SAMPLE_XLSX_FILE_PATH = "C:\\Users\\joaqu\\Documents\\Codigo\\Excel_to_prolog\\data\\Proyecto.xlsx";
-    // Creating a Workbook from an Excel file (.xls or .xlsx)
     private static Workbook workbook;
     private static List<String> lines= new ArrayList<String>();
-
     private static DataFormatter dataFormatter = new DataFormatter();
     static {
         try {
@@ -29,58 +25,55 @@ public class Main {
         }
     }
     private static Sheet sheet = workbook.getSheetAt(0);
+    //CRNs are the id for each group
     private static String[] CRNs = new String[sheet.getLastRowNum()];
 
-    private static String decapitalize(String string){
-        if (string == null || string.length() == 0) {
-            return string;
-        }
-        char[] c = string.toCharArray();
-        c[0] = Character.toLowerCase(c[0]);
-        return new String(c);
-    }
-    private static String camelCase(String in){
-        return WordUtils.capitalizeFully(in, ' ').replaceAll(" ", "");
-    }
-    private static void create_relations(int[] rows_to_read,String[] relations,int l){
-        int i;
-        String aux="";
-        for(i=0;i<l;++i){
-            aux = dataFormatter.formatCellValue(sheet.getRow(0).getCell(rows_to_read[i]));
-            relations[i]= CRN +"_"+camelCase(aux);
-        }
-    }
-
-    //TODO: Recursive Action
-//    private static void process(String[][] data,int n,int m,String[] relations){
-//        int i,j;
-//        for (i = 0; i < n; ++i){
-//            for(j=0;j<m;++j){
-//                if (data[i][j].contains(" ")||data[i][j].contains("+")||data[i][j].contains("@")) {
-//                    data[i][j] = "\"" + data[i][j] + "\"";
-//                } else {
-//                    data[i][j] = data[i][j].toLowerCase();
-//                }
-//                data[i][j]=relations[i]+"("+CRNs[j]+","+data[i][j]+").";
-//            }
-//        }
-//    }
     public static void main(String[] args) throws IOException, InvalidFormatException {
-        int[] rows_to_read = new int[]{7,19,20};
+        int i,j;
+        int[] rows_to_read = new int[]{7,19,20,21,24,29};
         int n_rows_to_read =rows_to_read.length;
-        String[] relations = new String[n_rows_to_read];
         String[][] data = new String[n_rows_to_read][sheet.getLastRowNum()];
-        create_relations(rows_to_read,relations,n_rows_to_read);
+        String[] relations = new String[n_rows_to_read];
+        String id_name="crn";
 
-        String relation="";
+        create_relations(rows_to_read,relations,n_rows_to_read,id_name);
+        add_headers(n_rows_to_read, relations);
+        read_data(rows_to_read, n_rows_to_read, data);
+
+        //process data
+        Process p = new Process(0,sheet.getLastRowNum(),n_rows_to_read,data,relations,CRNs);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(p);
+        pool.shutdown();
+
+        write_data(n_rows_to_read, data);
+
+    }
+
+    private static void write_data(int n_rows_to_read, String[][] data) throws IOException {
+        int i;
+        int j;
+        for (i = 0; i < n_rows_to_read; ++i){
+            for(j=0;j<sheet.getLastRowNum();++j) {
+                lines.add(data[i][j]);
+            }
+        }
+        Path file = Paths.get("C:\\Users\\joaqu\\Documents\\Codigo\\Excel_to_prolog\\data\\data.pl");
+        Files.write(file, lines, StandardCharsets.UTF_8);
+        workbook.close();
+    }
+
+    private static void add_headers(int n_rows_to_read, String[] relations) {
         int i=0,j;
         lines.add(":- encoding(utf8).");
         for(i=0; i<n_rows_to_read;++i){
             lines.add(":- discontiguous "+relations[i]+"/2.");
         }
+    }
 
-        //read
-        i=0;
+    private static void read_data(int[] rows_to_read, int n_rows_to_read, String[][] data) {
+        int i=0,j;
+        int CRN_row = 5; //for this spreadsheet
         for(Row row: sheet){
             if(i>=1){
                 CRNs[i-1] = dataFormatter.formatCellValue(row.getCell(CRN_row));
@@ -90,24 +83,20 @@ public class Main {
             }
             i++;
         }
-
-        //process data
-        Process p = new Process(0,sheet.getLastRowNum(),n_rows_to_read,data,relations,CRNs);
-        ForkJoinPool pool = new ForkJoinPool();
-        pool.invoke(p);
-        pool.shutdown();
-        //process(data,n_rows_to_read,sheet.getLastRowNum(),relations);
-
-        //write
-        for (i = 0; i < n_rows_to_read; ++i){
-            for(j=0;j<sheet.getLastRowNum();++j) {
-                lines.add(data[i][j]);
-            }
-        }
-        Path file = Paths.get("C:\\Users\\joaqu\\Documents\\Codigo\\Excel_to_prolog\\data\\data.pl");
-        Files.write(file, lines, StandardCharsets.UTF_8);
-
-        //close
-        workbook.close();
     }
+
+    private static String camelCase(String in){
+        return WordUtils.capitalizeFully(in, ' ').replaceAll(" ", "");
+    }
+
+    private static void create_relations(int[] rows_to_read,String[] relations,int l,String id){
+        int i;
+        String aux="";
+        for(i=0;i<l;++i){
+            aux = dataFormatter.formatCellValue(sheet.getRow(0).getCell(rows_to_read[i]));
+            relations[i]= id+"_"+camelCase(aux);
+        }
+    }
+
+
 }
--- java/src/Process.java
+++ java/src/Process.java
@@ -5,27 +5,27 @@ public class Process extends RecursiveAction {
     private int start, end, n_rows_to_read;
     private String[][] data;
     private String[] relations;
-    private String[] crns;
+    private String[] id_list;
 
-    public Process(int start, int end, int n_rows_to_read, String[][] data, String[] relations,String[] crns) {
+    Process(int start, int end, int n_rows_to_read, String[][] data, String[] relations, String[] id_list) {
         this.start = start;
         this.end = end;
         this.n_rows_to_read = n_rows_to_read;
         this.data = data;
         this.relations = relations;
-        this.crns = crns;
+        this.id_list = id_list;
     }
 
     private void computeDirectly(){
         int i,j;
         for (i = 0; i < n_rows_to_read; ++i){
             for(j=start;j<end;++j){
-                if (data[i][j].contains(" ")||data[i][j].contains("+")||data[i][j].contains("@")) {
-                    data[i][j] = "\"" + data[i][j] + "\"";
-                } else {
+                if(data[i][j].matches("^[a-zA-Z0-9]+$")){
                     data[i][j] = data[i][j].toLowerCase();
+                }else{
+                    data[i][j] = "\"" + data[i][j] + "\"";
                 }
-                data[i][j]=relations[i]+"("+crns[j]+","+data[i][j]+").";
+                data[i][j]=relations[i]+"("+ id_list[j]+","+data[i][j]+").";
             }
         }
     }
@@ -36,8 +36,8 @@ public class Process extends RecursiveAction {
             computeDirectly();
         }else{
             int mid = (start+end)>>>1;
-            invokeAll(new Process(start,mid, n_rows_to_read,data,relations,crns),
-                    new Process(mid,end, n_rows_to_read,data,relations,crns));
+            invokeAll(new Process(start,mid, n_rows_to_read,data,relations, id_list),
+                    new Process(mid,end, n_rows_to_read,data,relations, id_list));
         }
     }
 }
