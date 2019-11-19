b9ab32bcda1491973051b331e3fe0b0e579b1d75
97joaquinhr <97joaquinhr@gmail.com>
2019-11-19 10:44:07 -0600

    Added query for checking all the courses that a teacher has

--- queries.pl
+++ queries.pl
@@ -1,5 +1,23 @@
 :-include('info/data.pl').
 
+%helper append function
+appendM([],List1,List1).
+appendM([Head|Tail],List2,[Head|Result]):-
+    append(Tail, List2, Result).
+
 nombre_apellidoP(Nomina,Nombre,Apellido):-
     nomina_Nombrep(Nomina,Nombre),
     nomina_Apaternop(Nomina,Apellido).
+
+cursosH([],X,X):-!.
+
+cursosH([H|T],X,Res):-
+    crn_NombreMateria(H,Curso),
+    appendM(X,[Curso],NewX),
+    cursosH(T,NewX,Res).
+
+cursos(Nomina,X):-
+    findall(CRN,crn_Nómina(CRN,Nomina),Aux),
+    cursosH(Aux,[],X).
+    /*findall(Curso,crn_NombreMateria(CRN,Curso),X).*/
+
